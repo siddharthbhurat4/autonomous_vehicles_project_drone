e07afabfec0e0f6ecacf1a2997d31e7b7a3280a0
siddharthbhurat04 <bhuratsiddharth@gmail.com>
2023-10-01 02:03:50 -0400

    made few corrections in the eom's, added few symbolic variables, need to figure out a way to calculate the jacobian.

--- eom.m
+++ eom.m
@@ -1,17 +1,33 @@
 function sdot = eom(t, s)
 
-m = 0.468;
+syms x y z real
+syms u v w real
+syms phi theta phsi real %euler angles
+syms pdot qdot rdot real
+syms udot vdot wdot real 
+syms p q r real
+syms m g Ix Iy Iz real 
+syms K l b Ax Ay Az real %k-lift constant, b- drag constant
+syms omega1 omega2 omega3 omega4 real
+
+m = 0.6461009174; %0.468;
 g = 9.81;
-Ix = 4.856*1e-3;
-Iy = 4.856*1e-3;
-Iz = 8.801*1e-3;
-l = 0.225;
-K = 2.980*1e-6;
+Ix = 7.5*(10^-3);%4.856*1e-3;
+Iy = 7.5*(10^-3);%4.856*1e-3;
+Iz = 1.3^(10^-2);%8.801*1e-3;
+l = 0.23; %0.225;
+K = 3.13*(10^-5);%2.980*1e-6;
 b = 1.14*1e-7;
 % params.Ax = 0.25*0;
 % params.Ay = 0.25*0;
 % params.Az = 0.25*0;
 
+omega = sqrt((m*g/4)/K);
+dspeed = 0.05*omega;
+omega1 = omega+(1.5*dspeed);
+omega2 = omega-(1.5*dspeed);
+omega3 = omega+(1.5*dspeed);
+omega4 = omega-(1.5*dspeed);
 
 %state vars
 x = s(1);
@@ -22,40 +38,35 @@ v = s(5);
 w = s(6);
 phi = s(7);
 theta = s(8);
-psi = s(9);
+phsi = s(9);
 p = s(10);
 q = s(11);
 r = s(12);
 
-rotm = eul2rotm([phi, theta, psi],"ZYX");
-lin_vel = rotm*[u v w]';
+F = K*(omega1^2 + omega2^2 + omega3^2 + omega4^2);
+tau_phi = K*l*(omega4^2-omega2^2);
+tau_theta = K*l*(omega3^2-omega1^2);
+tau_psi = b*(omega1^2+omega2^2+omega3^2+omega4^2);
 
-omega = ones(4,1)*m*g/4;
-disp(omega);
-% omega = omega + [0;1;0;-1]; %roll
-% omega = omega - [-1;0;1;0]; %pitch
-% omega = omega - [-1;1;-1;1]; %yaw
+%Translational Kinematics
+rotm_transkin = [cos(phsi)*cos(theta), cos(phsi)*sin(theta)*sin(phi)-sin(phsi)*cos(phi), cos(phsi)*sin(theta)*cos(phi)+sin(phsi)*sin(phi);
+                 sin(phsi)*cos(theta), sin(phsi)*sin(theta)*sin(phi) + cos(phsi)*cos(phi), sin(phsi)*sin(theta)*cos(phi)-cos(phsi)*sin(phi);
+                 -sin(theta), cos(theta)*sin(phi), cos(theta)*cos(phi)];
+lin_vel = rotm_transkin*[u v w]';
 
-% params.omega1 = speed-0.5*dspeed;
-% params.omega2 = speed+0.5*dspeed;
-% params.omega3 = speed-0.5*dspeed;
-% params.omega4 = speed+0.5*dspeed;
+% translational dynamics
+udot = r*v-q*w + g*sin(theta);
+vdot = p*w-r*u - g*cos(theta)*sin(phi);
+wdot = q*u-p*v - g*cos(theta)*cos(phi) + F/m;
 
-F = sum(omega);
-tau_phi = K*l*(omega(4)^2-omega(2)^2);
-tau_theta = K*l*(omega(3)^2-omega(1)^2);
-tau_psi = b*(omega(1)^2-omega(2)^2+omega(3)^2-omega(4)^2);
+%Rotational Kinematics
+rotm_rotkin = [1 sin(phi)*tan(theta) cos(phi)*tan(theta);
+               0 cos(phi) -sin(phi);
+               0 sin(phi)/cos(theta) cos(phi)/cos(theta)];
+angular_vel = rotm_rotkin*[p q r]';
+disp(size(angular_vel))
 
-
-udot = r*v-q*w + -g*sin(theta);
-vdot = p*w-r*u + g*cos(theta)*sin(phi);
-wdot = q*u-p*v + g*cos(theta)*cos(phi) + 1/m*-F;
-
-rot_rotm = [1 sin(phi)*tan(theta) cos(phi)*tan(theta)
-           0 cos(phi) -sin(phi)
-           0 sin(phi)/cos(theta) cos(phi)/cos(theta)];
-angular_vel = rot_rotm*[p q r]';
-
+% rotational dynamics
 pdot = ((Iy - Iz)/Ix)*q*r + tau_phi/Ix;
 qdot = ((Iz - Ix)/Iy)*p*r + tau_theta/Iy;
 rdot = ((Ix - Iy)/Iz)*p*q + tau_psi/Iz;
