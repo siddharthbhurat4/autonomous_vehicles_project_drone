61e325bb4b78fae303c06f2084f7a131ab72ecd9
siddharthbhurat04 <bhuratsiddharth@gmail.com>
2023-10-01 21:33:56 -0400

    jacobian working with symbolic variables

--- eom.m
+++ eom.m
@@ -1,5 +1,4 @@
 function sdot = eom(t, s)
-
 syms x y z real
 syms u v w real
 syms phi theta phsi real %euler angles
@@ -8,40 +7,39 @@ syms udot vdot wdot real
 syms p q r real
 syms m g Ix Iy Iz real 
 syms K l b Ax Ay Az real %k-lift constant, b- drag constant
-syms omega1 omega2 omega3 omega4 real
-
-m = 0.6461009174; %0.468;
-g = 9.81;
-Ix = 7.5*(10^-3);%4.856*1e-3;
-Iy = 7.5*(10^-3);%4.856*1e-3;
-Iz = 1.3^(10^-2);%8.801*1e-3;
-l = 0.23; %0.225;
-K = 3.13*(10^-5);%2.980*1e-6;
-b = 1.14*1e-7;
+syms omega omega1 omega2 omega3 omega4 real
+% m = 0.6461009174; %0.468;
+% g = 9.81;
+% Ix = 7.5*(10^-3);%4.856*1e-3;
+% Iy = 7.5*(10^-3);%4.856*1e-3;
+% Iz = 1.3^(10^-2);%8.801*1e-3;
+% l = 0.23; %0.225;
+% K = 3.13*(10^-5);%2.980*1e-6;
+% b = 1.14*1e-7;
 % params.Ax = 0.25*0;
 % params.Ay = 0.25*0;
 % params.Az = 0.25*0;
 
 omega = sqrt((m*g/4)/K);
 dspeed = 0.05*omega;
-omega1 = omega+(1.5*dspeed);
-omega2 = omega-(1.5*dspeed);
-omega3 = omega+(1.5*dspeed);
-omega4 = omega-(1.5*dspeed);
+omega1 = omega;%+(0.5*dspeed);
+omega2 = omega;%-(0.5*dspeed);
+omega3 = omega-dspeed;%+(0.5*dspeed);
+omega4 = omega;%-(0.5*dspeed);
 
 %state vars
-x = s(1);
-y = s(2);
-z = s(3);
-u = s(4);
-v = s(5);
-w = s(6);
-phi = s(7);
-theta = s(8);
-phsi = s(9);
-p = s(10);
-q = s(11);
-r = s(12);
+% x = s(1);
+% y = s(2);
+% z = s(3);
+% u = s(4);
+% v = s(5);
+% w = s(6);
+% phi = s(7);
+% theta = s(8);
+% phsi = s(9);
+% p = s(10);
+% q = s(11);
+% r = s(12);
 
 F = K*(omega1^2 + omega2^2 + omega3^2 + omega4^2);
 tau_phi = K*l*(omega4^2-omega2^2);
@@ -64,14 +62,13 @@ rotm_rotkin = [1 sin(phi)*tan(theta) cos(phi)*tan(theta);
                0 cos(phi) -sin(phi);
                0 sin(phi)/cos(theta) cos(phi)/cos(theta)];
 angular_vel = rotm_rotkin*[p q r]';
-disp(size(angular_vel))
 
 % rotational dynamics
 pdot = ((Iy - Iz)/Ix)*q*r + tau_phi/Ix;
 qdot = ((Iz - Ix)/Iy)*p*r + tau_theta/Iy;
 rdot = ((Ix - Iy)/Iz)*p*q + tau_psi/Iz;
 
-sdot = zeros(12,1);
+% sdot = zeros(12,1);
 sdot(1) = lin_vel(1);
 sdot(2) = lin_vel(2);
 sdot(3) = lin_vel(3);
@@ -84,6 +81,8 @@ sdot(9) = angular_vel(3);
 sdot(10) = pdot;
 sdot(11) = qdot;
 sdot(12) = rdot;
+
 state_vector = [x,y,z,u,v,w,phi,theta,phsi,p,q,r];
-A = jacobian(sdot,state_vector)
+A = jacobian(sdot,state_vector);
+disp(A)
 end
