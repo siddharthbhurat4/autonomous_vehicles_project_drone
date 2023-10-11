d6acde64085203f95e5f6fcff641323189fea1bb
Marco <marcovc41@gmail.com>
2023-10-11 16:50:21 -0400

    Simplified K

--- run_sim.m
+++ run_sim.m
@@ -1,7 +1,8 @@
 clear;
 close all;
-s0 = ones(12,1);   % state initial conditions
-%s0 = [0;4;0;0;0;0;0;0;0;0;0;0];
+%s0 = ones(12,1);   % state initial conditions
+s0 = [0;3;0;0;0;0;0;0;0;0;0;0];
+%s0 = [0;2;0;0;0;0;0;0;0;0;0;-2];
 %s0 = [0;0;0;0;0;0;0;0;0;0;0;0];
 T = 10;             % time horizon (sec)
 
@@ -19,7 +20,9 @@ p(8).LineStyle = '-.';
 p(9).LineStyle = '-.';
 legend('x','y','z','$\phi$','$\theta$','$\psi$','$\dot{x}$','$\dot{y}$','$\dot{z}$','$\dot{\phi}$','$\dot{\theta}$','$\dot{\psi}$','Interpreter','latex','Fontsize',12)
 
+
 figure(2)
+pause(0.05)
 positions = s(:,1:3);
 angles = s(:,7:9);
 animate(positions,angles);
